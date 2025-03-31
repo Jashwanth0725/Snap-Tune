b35343e029ebbf54a59f7454693bb17cf0fe6b62
Tandamalla Jashwanth Sai <jashwanthsai07251@gmail.com>
2025-04-01 00:01:32 +0530

    chore(deps): Installed cors, cookie-parser

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,31 +1,14 @@
 import dbconnect from './db/index.js';   //Importing database connectioning function
 import dotenv from 'dotenv';            // Importing dotenv to use environment variables
 dotenv.config({ path: '../.env' });
-import express from 'express';         // Importing express to create server
+import express from 'express';           // Importing express to create server
 const app = express()
 
-const PORT = process.env.PORT || 5000
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+const PORT = process.env.PORT || 5000  // Port number for the server
 
 app.get('/Jashu', (req, res) => {
     res.send("Hi Jashwanth. This is test of server");
 })
-
-app.get('/testing', (req, res) => {
-    res.send("This is testing page. Working successfully, Just checking if the nodemon is running properl");
-})
-
-app.get('/jokes', (req, res) => {
-    const jokes = [
-        { "joke": "Why did the chicken cross the road?" },
-        { "joke": "Knock, knock. Who’s there?" }
-    ];
-    res.json(jokes);
-})
-
 
 dbconnect();
 
