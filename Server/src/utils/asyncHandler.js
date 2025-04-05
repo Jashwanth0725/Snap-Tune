0bb38c9c103920239771c55f7163866e297c2a9c
Tandamalla Jashwanth Sai <jashwanthsai07251@gmail.com>
2025-04-06 00:17:11 +0530

    fix(bugs): Fixed minor bugs in utils files

--- Server/src/utils/asyncHandler.js
+++ Server/src/utils/asyncHandler.js
@@ -1,12 +1,13 @@
 // This utility function wraps an asynchronous request handler function 
 // and catches any errors that occur during its execution.
 
-const asyncHandler = (requestHandler) => (req, res, next) => {
-    Promise.resolve(requestHandler(req, res, next)).catch((error) => {
-        console.error(error);
-        next(error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    });
-}
-
-export default asyncHandler;
+const asyncHandler = (requestHandler) =>
+    (req, res, next) => {
+        return (
+            Promise.resolve(requestHandler(req, res, next)).catch((error) => {
+                console.error(error);
+                next(error);
+                res.status(500).json({ error: 'Internal Server Error' });
+            }));
+    }
+export default asyncHandler; 
