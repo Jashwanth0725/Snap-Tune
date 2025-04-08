03e6c7cb07765e6c7477a51d4d20863518a0e270
Tandamalla Jashwanth Sai <jashwanthsai07251@gmail.com>
2025-04-09 02:11:46 +0530

    feat(auth) : Created authentication middleware for user

---
+++ Server/src/middlewares/auth.middleware.js
@@ -0,0 +1,31 @@
+import asyncHandler from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+import userdetails from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+
+
+const verifyJWT = asyncHandler(async (req, res, next) => {
+    try {
+        const accessToken = req.cookies?.accesToken || req.header("Authorization")?.replace("Bearer ", "");
+
+        if (!accessToken) {
+            throw new ApiError(401, "Unauthorized request");
+        }
+
+        const decodedToken = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+
+        const user = await userdetails.findById(decodedToken?._id).select("-password -refreshToken");
+
+        if (!user) {
+            throw new ApiError(401, "Invalid Access Token");
+        }
+
+        req.user = user;
+        next();
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid Access Token");
+    }
+});
+
+
+export { verifyJWT };
