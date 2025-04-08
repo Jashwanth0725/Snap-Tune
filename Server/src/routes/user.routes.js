4a679cffc597ab730e36803493b64d571e5f630e
Tandamalla Jashwanth Sai <jashwanthsai07251@gmail.com>
2025-04-09 02:10:16 +0530

    feat(routes) : created routes for user register, login, logout

--- Server/src/routes/user.routes.js
+++ Server/src/routes/user.routes.js
@@ -1,17 +1,22 @@
 import { Router } from 'express';
-import { registerUser } from '../controllers/user.controller.js'; // Importing the register controller
+import { registerUser, loginUser, logOutUser } from '../controllers/user.controller.js'; // Importing the register controller
 import { upload } from '../middlewares/multer.middleware.js';
-
+import { verifyJWT } from '../middlewares/auth.middleware.js';
 const userRouter = Router();
 
 userRouter.route('/register').post(
     upload.fields([
         {
             name: "profilePic",
-            maxCount: 1
+            maxCount: 1,
         }
     ]),
     registerUser
 );
+
+userRouter.route('/login').post(loginUser);
+
+//secured routes
+userRouter.route("/logout").post(verifyJWT, logOutUser);
 
 export default userRouter;
