9ce09c127fe18fd85c8ffafe3bc48571eb10d149
Tandamalla Jashwanth Sai <jashwanthsai07251@gmail.com>
2025-04-09 02:15:07 +0530

    fix(bugs) : Fixed bugs in app.js, multer middlewareand  public/uploads

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,9 +1,9 @@
 import express from 'express';
 
 import cors from 'cors';                     // Importing cors to handle cross-origin requests
-// import cookieParser from 'cookie-parser'; // Importing cookie-parser to parse cookies 
+import cookieParser from 'cookie-parser'; // Importing cookie-parser to parse cookies 
 
-const app = express();                      // Creating an instance of express
+const app = express();                       // Creating an instance of express
 
 app.use(cors({
     origin: `${process.env.CORS_ORIGIN}`, // Allowing requests from the specified origin
@@ -13,7 +13,7 @@ app.use(cors({
 app.use(express.json({ limit: '20kb' }));    // Parsing JSON requests with a limit of 20kb
 app.use(express.urlencoded({ extended: true, limit: '20kb' })); // Parsing URL-encoded requests
 app.use(express.static('public'));       // Serving static files from the 'public' directory
-
+app.use(cookieParser());
 
 //routes imports 
 import userRouter from './routes/user.routes.js';
--- Server/src/middlewares/multer.middleware.js
+++ Server/src/middlewares/multer.middleware.js
@@ -1,13 +1,21 @@
 import multer from 'multer';
+import fs from 'fs';
+// import path from 'path';
+
+// Ensure uploads directory exists
+const uploadDir = './public/uploads';
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, './public/uploads/');
+        cb(null, uploadDir);
     },
     filename: (req, file, cb) => {
-        // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        // cb(null, uniqueSuffix + '-' + file.originalname);
-        cb(null, file.originalname);
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, uniqueSuffix + '-' + file.originalname);
     }
 });
+
 export const upload = multer({ storage: storage });
