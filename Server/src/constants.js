7e06c2ac53f9b30a92f20de4be5d5b2e038c63d3
Tandamalla Jashwanth Sai <jashwanthsai07251@gmail.com>
2025-04-14 00:28:37 +0530

    fix(token): Changed access token expire from minute to day

--- Server/src/constants.js
+++ Server/src/constants.js
@@ -1,3 +1,3 @@
 export const DB_NAME = "Snap_Tune_Database";
-export const ACCESS_TOKEN_EXPIRE_IN_MINUTES = 60;
+export const ACCESS_TOKEN_EXPIRE_IN_DAYS = "1d";
 export const REFRESH_TOKEN_EXPIRE_IN_DAYS = "10d";
