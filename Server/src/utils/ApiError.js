ceff11add5d27495a729b38a79d6ca5ff15a7c5e
Tandamalla Jashwanth Sai <jashwanthsai07251@gmail.com>
2025-04-14 00:36:15 +0530

    feat(ApiError): added data parameter to customize error payloads

--- Server/src/utils/ApiError.js
+++ Server/src/utils/ApiError.js
@@ -1,10 +1,10 @@
 class ApiError extends Error {
 
     // Constructor for the ApiError class, which extends the built-in Error class.
-    constructor(statusCode, message = "Something went wrong", error = [], stack = "") {
+    constructor(statusCode, message = "Something went wrong", error = [], stack = "", data = null) {
         super(message);                             // Call the parent constructor of Error with the message
         this.statusCode = statusCode;               // Set the HTTP status code
-        this.data = null;
+        this.data = data;
         this.error = error;                         // Set the error details (if any)
         this.message = message;                     // Set the error message
         this.success = false;                       // Indicate that this is an error response
