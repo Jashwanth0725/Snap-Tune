3163d2f40050dde3e2725bdf8cb3e50635400eb6
JASHWANTH SAI TANDAMALLA <jashwanthsai07251@gmail.com>
2025-04-20 21:35:45 +0530

    feat(Api): create api's for caption, summary, stripe

---
+++ client/src/api/caption.api.tsx
@@ -0,0 +1,57 @@
+import { toast } from "sonner";
+
+const generateCaptions = async (image: File): Promise<string> => {
+  const formData = new FormData();
+  formData.append("image", image);
+
+  try {
+    const response = await fetch(
+      "http://localhost:8000/api/v1/generate/caption",
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Failed to generate caption");
+    }
+
+    const data = await response.json();
+    toast.success("Caption Generated Succesfully!");
+    return data.data;
+  } catch (error) {
+    toast.error("Something went wrong!");
+    console.error("Error generating caption:", error);
+    return "Something went wrong";
+  }
+};
+
+const generateAnotherCaption = async (oldCaption: string): Promise<string> => {
+  const formData = new FormData();
+  formData.append("oldCaption", oldCaption);
+
+  try {
+    const response = await fetch(
+      "http://localhost:8000/api/v1/generate/re-caption",
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Failed to generate caption");
+    }
+
+    const data = await response.json();
+    toast.success("Caption Re-Generated Succesfully!");
+    return data.data;
+  } catch (error) {
+    toast.error("Something went wrong!");
+    console.error("Error generating caption:", error);
+    return "Something went wrong";
+  }
+};
+
+export { generateCaptions, generateAnotherCaption };
