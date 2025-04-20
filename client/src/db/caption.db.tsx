8c8d830161b41cf6b5e05b175d58625bf1e007ff
JASHWANTH SAI TANDAMALLA <jashwanthsai07251@gmail.com>
2025-04-20 21:33:55 +0530

    feat(Database): create post-database files for caption, summary, user

---
+++ client/src/db/caption.db.tsx
@@ -0,0 +1,35 @@
+// import { useAuth } from "@/auth/firebase.auth";
+import { toast } from "sonner";
+// const { user } = useAuth();
+
+export const saveCaption = async (
+  userId: string,
+  image: File,
+  caption: string
+): Promise<string> => {
+  const formData = new FormData();
+
+  formData.append("userId", userId);
+  formData.append("image", image);
+  formData.append("caption", caption);
+
+  try {
+    const response = await fetch("http://localhost:8000/api/v1/users/caption", {
+      method: "POST",
+      body: formData,
+    });
+
+    if (!response.ok) {
+      toast.error("Failed to save in database");
+      throw new Error("Failed to save in Database");
+    }
+
+    const data = await response.json();
+    toast.success("Saved Successfully!");
+    return data.data;
+  } catch (error) {
+    toast.error("Something went wrong!");
+    console.error("Error saving caption:", error);
+    return "Something went wrong";
+  }
+};
