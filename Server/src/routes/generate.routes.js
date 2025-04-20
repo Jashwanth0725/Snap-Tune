ea27c2dc2f71c33aed1d2db1848d60a1dbc29bb8
JASHWANTH SAI TANDAMALLA <jashwanthsai07251@gmail.com>
2025-04-20 21:42:45 +0530

    feat(routes): Create routes for caption, summary

--- Server/src/routes/generate.routes.js
+++ Server/src/routes/generate.routes.js
@@ -1,12 +1,24 @@
 import { Router } from 'express';
 import { upload } from '../middlewares/multer.middleware.js';
-import { generateController } from '../controllers/generate.controller.js';
+import { captionController, reCaptionController, summaryController, reSummaryController } from '../controllers/generate.controller.js';
 
 const aiApiRouter = Router();
 
 aiApiRouter.route('/caption').post(
     upload.single('image'),
-    generateController
+    captionController
 )
+
+
+aiApiRouter.route('/re-caption').post(
+    upload.single('image'),
+    reCaptionController
+)
+
+aiApiRouter.route('/summary').post(summaryController)
+
+aiApiRouter.route('/re-summary').post(reSummaryController)
+
+
 
 export default aiApiRouter;
--- Server/src/routes/user.routes.js
+++ Server/src/routes/user.routes.js
@@ -1,25 +1,39 @@
 import { Router } from 'express';
-import { registerUser, loginUser, logOutUser, refreshAccessToken } from '../controllers/user.controller.js'; // Importing the register controller
+// import { registerUser, loginUser, logOutUser, refreshAccessToken } from '../controllers/user.controller.js'; // Importing the register controller
 import { upload } from '../middlewares/multer.middleware.js';
-import { verifyJWT } from '../middlewares/auth.middleware.js';
+// import { verifyJWT } from '../middlewares/auth.middleware.js';
+import { captionSaveDatabase, summarySaveDatabase, userSaveDatabase, userPayment } from '../controllers/database.controller.js';
+import { userHistory } from '../controllers/history.controller.js'
+
 const userRouter = Router();
 
-userRouter.route('/register').post(
-    upload.fields([
-        {
-            name: "profilePic",
-            maxCount: 1,
-        }
-    ]),
-    registerUser
-);
+// userRouter.route('/register').post(
+//     upload.fields([
+//         {
+//             name: "profilePic",
+//             maxCount: 1,
+//         }
+//     ]),
+//     registerUser
+// );
 
-userRouter.route('/login').post(upload.none()
-    , loginUser);
+// userRouter.route('/login').post(upload.none()
+//     , loginUser);
 
-//secured routes
-userRouter.route("/logout").post(verifyJWT, logOutUser);
+// //secured routes
+// userRouter.route("/logout").post(verifyJWT, logOutUser);
 
-userRouter.route("/refresh-token").post(refreshAccessToken);
+// userRouter.route("/refresh-token").post(refreshAccessToken);
 
-export default userRouter;
+
+userRouter.route("/userdetails").post(userSaveDatabase);
+
+userRouter.route("/caption").post(upload.single('image'), captionSaveDatabase);
+
+userRouter.route("/summary").post(summarySaveDatabase);
+
+userRouter.route("/payment").post(userPayment);
+
+userRouter.route("/history/:userId").get(userHistory);
+
+export default userRouter; ``
