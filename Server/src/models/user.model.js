e24036752462f565de89de5a30ecd9574329c738
JASHWANTH SAI TANDAMALLA <jashwanthsai07251@gmail.com>
2025-04-20 21:42:08 +0530

    feat(new models): create new models for summmary, caption, payment

--- Server/src/models/user.model.js
+++ Server/src/models/user.model.js
@@ -1,26 +1,19 @@
 import mongoose from "mongoose";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcryptjs";
-import { ACCESS_TOKEN_EXPIRE_IN_DAYS, REFRESH_TOKEN_EXPIRE_IN_DAYS } from "../constants.js";
+// import jwt from "jsonwebtoken";
+// import bcrypt from "bcryptjs";
+// import { ACCESS_TOKEN_EXPIRE_IN_DAYS, REFRESH_TOKEN_EXPIRE_IN_DAYS } from "../constants.js";
 
 const userSchema = new mongoose.Schema({
-    userName: {
+    googleId: {
         type: String,
         required: true,
-        unique: true,
-        lowercase: true,
-        trim: true,
-        index: true
+        unique: true
     },
     name: {
         type: String,
         required: true,
         lowercase: true,
         trim: true
-    },
-    profilePic: {
-        type: String,
-        default: "https://res.cloudinary.com/dr1ktryw0/image/upload/v1736097744/samples/people/boy-snow-hoodie.jpg"
     },
     email: {
         type: String,
@@ -29,56 +22,17 @@ const userSchema = new mongoose.Schema({
         lowercase: true,
         trim: true,
     },
-    password: {
+    profilePic: {
         type: String,
-        required: true,
-        min: [6, "Password must be atleast 6 letters long"],
-        max: 12,
+        default: "https://res.cloudinary.com/dr1ktryw0/image/upload/v1736097744/samples/people/boy-snow-hoodie.jpg"
     },
     history: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: "history",
-    }],
-    refreshToken: {
-        type: String
-    }
+    }]
 }, {
     timestamps: true
 });
-
-//Password will be encrypted before saving it to the database
-userSchema.pre("save", async function (next) {
-    if (this.isModified("password")) {
-        this.password = await bcrypt.hash(this.password, 10);
-    }
-    next();
-})
-
-//This compares the password enterd by user and the password stored in database.
-//This return true or false
-userSchema.methods.isPasswordCorrect = async function (password) {
-    return await bcrypt.compare(password, this.password);
-};
-
-userSchema.methods.createAccessToken = function () {
-    return jwt.sign({
-        _id: this._id,
-        userName: this.userName,
-        email: this.email,
-    }, process.env.ACCESS_TOKEN_SECERET, {
-        expiresIn: ACCESS_TOKEN_EXPIRE_IN_DAYS
-    })
-}
-
-//This method generate refresh token which is used to get new access token when the old one expires
-//This method is called when user login or registers
-userSchema.methods.createRefreshToken = function () {
-    return jwt.sign({
-        _id: this._id
-    }, process.env.REFRESH_TOKEN_SECERET, {
-        expiresIn: REFRESH_TOKEN_EXPIRE_IN_DAYS
-    });
-};
 
 const userdetails = mongoose.model("userdetails", userSchema);
 
