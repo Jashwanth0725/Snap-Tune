12bb6fde58b2347d29d55a26faa1d7cf88fbe929
JASHWANTH SAI TANDAMALLA <jashwanthsai07251@gmail.com>
2025-07-05 01:03:12 +0530

    Changed way of storing of image from in disk storage to memorystorage to encounter deployment issue

--- Server/src/middlewares/multer.middleware.js
+++ Server/src/middlewares/multer.middleware.js
@@ -1,21 +1,5 @@
 import multer from 'multer';
-import fs from 'fs';
-// import path from 'path';
 
-// Ensure uploads directory exists
-const uploadDir = './public/uploads';
-if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir, { recursive: true });
-}
+const storage = multer.memoryStorage();
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, uploadDir);
-    },
-    filename: (req, file, cb) => {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, uniqueSuffix + '-' + file.originalname);
-    }
-});
-
-export const upload = multer({ storage: storage });
+export const upload = multer({ storage });
