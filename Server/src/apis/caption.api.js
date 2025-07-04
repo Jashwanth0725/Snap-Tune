fac0aa373a895eaff4b61d1ace365c29b4885d31
JASHWANTH SAI TANDAMALLA <jashwanthsai07251@gmail.com>
2025-07-05 01:00:34 +0530

    Changed the way of sending the image to gemini

--- Server/src/apis/caption.api.js
+++ Server/src/apis/caption.api.js
@@ -1,23 +1,25 @@
 import { ApiError } from "../utils/ApiError.js";
-import { GoogleGenAI, createUserContent, createPartFromUri } from "@google/genai";
+import pkg from "@google/genai";
+const { GoogleGenAI, createUserContent } = pkg;
 
-
-
-const generateCaption = async (filePath) => {
+const generateCaption = async (fileBuffer, mimeType) => {
 
     try {
         const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
-        const image = await ai.files.upload({
-            file: filePath,
-        });
+        const image = {
+            inlineData: {
+                data: fileBuffer.toString('base64'),
+                mimeType: mimeType
+            }
+        };
 
         const response = await ai.models.generateContent({
             model: "gemini-2.0-flash",
             contents: [
                 createUserContent([
                     "Give 1 popular instagram caption with 4 hashtags, no introductory text",
-                    createPartFromUri(image.uri, image.mimeType),
+                    image
                 ]),
             ],
         });
